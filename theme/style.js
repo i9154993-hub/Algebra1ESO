0cd459ed1cce54cec6572ac5f8f9ddd7edee7022
i9154993-hub <i9154993@gmail.com>
2026-06-14 03:06:23 +0200

    Añado proyecto eXeLearning

---
+++ theme/style.js
@@ -0,0 +1,7 @@
+// Default theme JS
+(function() {
+  document.addEventListener('DOMContentLoaded', function() {
+    // Theme initialization
+    console.log('[Theme] Default theme loaded');
+  });
+})();
